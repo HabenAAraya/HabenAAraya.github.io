678a0c181e0e3dd0c943159ec7fe3ff5d617f727
habenAbrhaley <112903511+habenaraya@users.noreply.github.com>
2022-09-21 23:56:21 -0500

    node

--- node/use_my_math.js
+++ node/use_my_math.js
@@ -1,4 +1,4 @@
-const math = require('./my_math');
+const math = require('../my_math');
 console.log("3 + 5 = " + math.add(3, 5));
 console.log("3 - 5 = " + math.subtract(3, 5));
 console.log("3 * 5 = " + math.multiply(3, 5));
